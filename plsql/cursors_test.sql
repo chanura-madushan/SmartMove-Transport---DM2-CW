e57ea2789d7e3be321db4440926ce33cf98e6ea9
chanura-madushan <chanuramadushan18@gmail.com>
2026-09-25 22:20:22 +0530

    cursors tested

---
+++ plsql/cursors_test.sql
@@ -0,0 +1,21 @@
+
+SET SERVEROUTPUT ON;
+//1: Passenger Travel History
+BEGIN
+    PassengerTravelHistory(1);
+END;
+/
+
+
+//2: Vehicles Due For Maintenance
+BEGIN
+    VehiclesDueForMaintenance;
+END;
+/
+
+
+//3: Most Frequently Used Routes
+BEGIN
+    MostUsedRoutes;
+END;
+/
